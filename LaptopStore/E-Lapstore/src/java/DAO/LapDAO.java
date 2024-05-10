fe47bb7d266e75f740e33763fc51a14c9ca2959c
HirushiRathnayaka <118349206+HirushiRathnayaka@users.noreply.github.com>
2024-05-10 22:24:43 +0530

    Update LapDAO.java
    Add full code in to the LapDAO.java

--- LaptopStore/E-Lapstore/src/java/DAO/LapDAO.java
+++ LaptopStore/E-Lapstore/src/java/DAO/LapDAO.java
@@ -5,10 +5,36 @@
  */
 package DAO;
 
+import entity.LapDtls;
+import java.util.List;
+
 /**
  *
- * @author 15U560
+ * @author wwwkr
  */
-public class LapDAO {
+public interface LapDAO {
+    
+    public boolean addLaps(LapDtls b);
+    
+    public List<LapDtls> getAllLaps();    
+    
+    public LapDtls getLapById(int id);
+    
+    public boolean updateEditLap(LapDtls b);
+    
+    public boolean deleteLaps(int id);
+    
+    public List<LapDtls> getNewLap();
+    
+    public List<LapDtls> getRecentLap();
+    
+    public List<LapDtls> getOldLaps();
+    
+    public List<LapDtls> getAllRecentLaps();
+    
+    public List<LapDtls> getAllNewLaps();
+    
+    public List<LapDtls> getAllOldLaps();
+    
     
 }
