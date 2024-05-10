3f014d72234f62b4b3a5308b840782040c1d2acd
HirushiRathnayaka <118349206+HirushiRathnayaka@users.noreply.github.com>
2024-05-10 22:41:59 +0530

    Update LapDtls.java
    Updated LapDtls class to improve code structure and readability

--- LaptopStore/E-Lapstore/src/java/entity/LapDtls.java
+++ LaptopStore/E-Lapstore/src/java/entity/LapDtls.java
@@ -7,8 +7,32 @@ package entity;
 
 /**
  *
- * @author 15U560
+ * @author wwwkr
  */
 public class LapDtls {
     
+    private int lapid;
+    private String lapName;
+    private String price;
+    private String lapCategory;
+    private String status;
+    private String photoName;
+    private String email;
+    
+    public LapDtls(){
+        super();
+    }
+    public LapDtls(String lapName,String price,String lapCategory,String status,String photoName,String email)
+    {
+        super();
+        this.lapName=lapName;
+        this.price=price;
+        this.lapCategory=lapCategory;
+        this.status=status;
+        this.photoName=photoName;
+        this.email=email;
+    }
+    
+   
+    
 }
